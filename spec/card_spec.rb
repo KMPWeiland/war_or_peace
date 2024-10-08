c97ae8e47db23c010b88be7982dae35c164d88e5
Kristin Weiland <kristinweiland@gmail.com>
2024-10-08 16:21:02 -0700

    Edit and correct testing code in card_spec.rb.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,16 +3,16 @@ require './lib/card'
 
 RSpec.describe Card do
   it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new('diamond', 'Queen', 12)
 
     expect(card).to be_an_instance_of(Card)
   end
 
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new('diamond', 'Queen', 12)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    expect(card.suit).to eq('diamond')
+    expect(card.rank).to eq('Queen')
+    expect(card.value).to eq(12)
   end
 end
