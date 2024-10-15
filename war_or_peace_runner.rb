b6e8f7c5c3a0bcdf9e8344a26ac9bb1e75531a28
Kristin Weiland <kristinweiland@gmail.com>
2024-10-14 19:27:07 -0700

    Add a play_game method to Game Play runner file.

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,17 +1,16 @@
 require_relative 'lib/card' #REQUIRE VS. REQUIRE RELATIVE?
 require 'rspec'
-require_relative '../lib/deck'
-require_relative '../lib/player'
+require_relative 'lib/deck'
+require_relative 'lib/player'
+require_relative 'lib/game'
+require_relative 'lib/turn'
 require 'pry'
-
 
 #Create 52 Cards (A standard deck)
 
 # Array to hold all 52 Card objects (from the Card class)
 cards = []
-
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'] #suits array contains all four suits
-
 # A hash that maps the card value to its rank
 values_and_ranks = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, 
 '9' => 9, '10' => 10, 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14}
@@ -30,11 +29,29 @@ split_deck = shuffled_cards.length / 2
 split_deck1 = shuffled_cards[0...split_deck]
 split_deck2 = shuffled_cards[split_deck..-1]
 
-p split_deck1 #HOW TO PRINT A CLEAN ARRAY WITHOUT THE INSTANCE CODES/NUMBERS?
-p split_deck2
-
+deck1 = Deck.new(split_deck1)
+deck2 = Deck.new(split_deck2)
 
 #Create two players with the Decks you created
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
 
+game = Game.new(player1, player2)
+#player 1 -2 are two varaibles holding these two distinct player objects
+
+# puts "Type GO to start the game."
+# user_input = gets.chomp
+# puts user_input
+
+# def start
+#     if user_input == "GO"
+#         puts "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are Megan and Aurora. Type 'GO' to start the game!"
+#     else
+#         puts "Invalid response. Please type GO to start."
+#     end
+# end
+
+game.start
+#now just call all the methods in the order that mimics the gameplay
+
+# turn1 = Turn.new(player1,player2)
