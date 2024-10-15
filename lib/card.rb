2b8ef5c33756d789c7621b451133c3b0de56a270
Kristin Weiland <kristinweiland@gmail.com>
2024-10-14 19:26:18 -0700

    Updates to card class.

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,27 @@
 class Card
-    attr_reader :suit,
-                :value,
-                :rank
+    # attr_reader :suit,
+    #             :value,
+    #             :rank
 
     def initialize(suit, value, rank)
         @suit = suit # string for the card's suit
         @value = value  # string for the card's rank
         @rank = rank # integer for the card's numerical value
     end
+
+    def suit
+        binding.pry
+        @suit
+    end
+    
+    def value
+        @value
+    end
+
+    def rank
+        @rank
+    end
+
+
+      
 end
